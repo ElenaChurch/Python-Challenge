9857e04f0171ee103656e43e6457e82d21c44122
unknown <you@example.com>
2021-10-09 12:36:58 -0400

    hw

---
+++ PyBank/main.py
@@ -0,0 +1,61 @@
+#import csv and os
+import os
+import csv
+
+#path for file
+cvspathpybank=os.path.join("Resources","budget_data.csv")
+
+#open with and creating path
+
+with open(cvspathpybank) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header=next(csvreader)
+
+    #putting varables to 0 to prepare to calculate 
+    month=0 
+    total=0
+    prev_rev = 0
+    ch_total = 0
+    pre_change=0
+    pre_change_min=0
+    max_date=0
+    min_date=0
+
+    # for loop to count the number of month (rows) and make the date into a list
+    for i,row in enumerate(csvreader):
+        rev =  float(row[1])
+        total+= rev
+        month += 1 
+        #getting rid of the first change bc it is zero
+        if i == 0:
+            prev_rev = rev
+        
+        #finding average change
+        change = rev - prev_rev
+        ch_total += change
+        prev_rev = rev
+
+        #finding greatest profit increase
+        if change > pre_change:
+            pre_change=change
+            max_date= row[0]
+
+        #finding decrease in profits
+        if change < pre_change_min:
+            pre_change_min = change
+            min_date= row[0]
+#output 
+output = f'''
+     Financial Analysis
+  ----------------------------
+  Total Months: {month}
+  Total: ${total:,.0f}
+  Average  Change: $ {ch_total/(month-1):,.2f}
+  Greatest Increase in Profits: {max_date} ({pre_change:,.2f})
+  Greatest Decrease in Profits: {min_date}({pre_change_min:,.2f})
+ '''
+
+print(output)
+
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,66 @@
+#import csv and os
+import os
+import csv
+
+#path for file
+cvspathpypoll=os.path.join("Resources","election_data.csv")
+
+#open with and creating path
+
+with open(cvspathpypoll) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header=next(csvreader)
+
+    # setting varables to 0 to prepare to calculate
+    totalvotes=0
+    khan=0
+    Correy=0
+    Li=0
+    Otooley=0
+    winner=""
+    Unique_list=[]
+   
+    #loop for counting
+    for row in csvreader:
+        totalvotes+= 1
+        if row[2] not in Unique_list:
+            Unique_list.append(row[2])
+        if str(row[2])=="Khan":
+            khan+=1
+        if str(row[2])=="Correy":
+            Correy+=1
+        if str(row[2])=="Li":
+            Li+=1
+        if str(row[2])=="O'Tooley":
+            Otooley+=1
+if khan > Li and khan > Correy and khan > Otooley:
+     winner="Khan"
+elif Li> khan and Li > Correy and Li > Otooley:
+    winner="Li"
+elif Correy > khan and Correy > Li and Correy > Otooley:
+    winner="Correy"
+elif Otooley > khan and Otooley > Li and Otooley > Correy:
+    winner="O'Tooley"
+
+khanpercent= (khan/totalvotes)*100       
+correypercent=(Correy/totalvotes)*100
+lipercent=(Li/totalvotes)*100
+otooleypercent=(Otooley/totalvotes)*100
+
+
+#printing output
+print(Unique_list)
+output=f'''text
+  Election Results
+  -------------------------
+  Total Votes: {totalvotes}
+  -------------------------
+  Khan: {khanpercent:,.3f}% ({khan})
+  Correy: {correypercent:,.3f}% ({Correy})
+  Li: {lipercent:,.3f}% ({Li})
+  O'Tooley: {otooleypercent:,.3f}% ({Otooley})
+  -------------------------
+  Winner: {winner}
+  -------------------------
+  '''
+print(output)
